b62c8fe86510b9d2558c1046fadbbac216bf2b93
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-09-07 04:38:10 +0800

    Revert "Revert "Revert "Revert "Update 7/9/2020""""
    This reverts commit 0334bb315ed738d7e6b8af4cbe578c0f736eff4f.

---
+++ SlimeCorp/Assets/Script/CustomerAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerAttribute : MonoBehaviour
+{
+    Animator anim;
+
+    public Vector3 destination;
+    public float speed;
+    Vector3 InsideShopLocation = new Vector3(3f, 7.8f);
+    Vector3 LeavingShopLocation = new Vector3(2f, 7.8f);
+    Vector3 deleteColliderLocation = new Vector3(20f, 7.8f);
+
+    private bool alreadyPurchase = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+        float distTemp = Vector3.Distance(destination, transform.position);
+        anim.SetFloat("Speed", distTemp);
+
+        if (transform.position == InsideShopLocation && alreadyPurchase == false)
+        {
+            speed = 0;
+            destination = LeavingShopLocation;
+            transform.position = LeavingShopLocation;
+            StartCoroutine(Shopping());
+        }
+
+        if (transform.position == deleteColliderLocation)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator Shopping()
+    {
+        yield return new WaitForSeconds(10f);
+        //Buy function;
+        alreadyPurchase = true;
+        
+        speed = 2f;
+        destination = deleteColliderLocation;
+    }
+}
