53b0332158cdaab639bcf2a9aca4aae3eb4e494c
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-09-13 02:06:29 +0800

    done shopping system gua

--- SlimeCorp/Assets/Script/ShoppingSystem.cs
+++ SlimeCorp/Assets/Script/ShoppingSystem.cs
@@ -6,32 +6,38 @@ using UnityEngine.UI;
 public class ShoppingSystem : MonoBehaviour
 {
     [Header("Stock Button")]
-    [SerializeField] Button[] ItemButton;
-    [SerializeField] Image[] ItemButtonImage;
+    public Button[] StockButton;
+    //public Image[] StockButton_Image;
 
     [Header("SlimeTypeQuantity")]
-    [SerializeField] Text[] SlimeTypeQuantityText;
+    public Text[] SlimeTypeQuantity_Text;
 
     [Header("Have Stock")]
-    [SerializeField] GameObject[] ItemDetails_HaveStock;
-    [SerializeField] Slider[] ItemSellingBar;
-    [SerializeField] Text[] RemainStockText;
-    public int[] RemainStockNumber = { 0, 1500, 2000, 2500};
-    int[] MaxStockNumber = { 1000, 1500, 2000, 2500 };
-    [SerializeField] Text[] ItemSellingPriceText;
-    [SerializeField] int[] ItemSellingPrice = { 10, 15, 35, 50 };
+    public GameObject[] StockDetails_HaveStock;
+    public Slider[] StockSelling_Bar;
+    public Text[] StockSelling_Text;
+    [SerializeField] int[] StockSelling_Number = { 0, 0, 0, 0 };
+    [SerializeField] int[] MaxStock_Number = { 500, 1000, 1500, 2000 };
+    public Text[] StockSellingPrice_Text;
+    [SerializeField] int[] StockSellingPrice_Number = { 10, 15, 35, 50 };
 
     [Header("Out Of Stock")]
-    [SerializeField] GameObject[] ItemDetails_OutOfStock;
+    public GameObject[] StockDetails_OutOfStock;
+    [SerializeField] int[] RestockCost_SlimeTypeCount = { 30, 45, 60, 80 };
+    [SerializeField] string[] SlimeType = { "Basic Slime", "Wood Slime", "Lava Slime", "Tek Slime" };
+    public Text[] RestockCost_Text;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 1; i < 4; i++)
+        GameObject.Find("StockPopup").SetActive(false);
+        for (int i = 1; i < 4; i++)
         {
-            ItemButton[i].gameObject.SetActive(false);
+            StockButton[i].gameObject.SetActive(false);
         }
+
+
     }
 
     // Update is called once per frame
@@ -39,78 +45,137 @@ public class ShoppingSystem : MonoBehaviour
     {
         CheckingUnlockLab();
         CheckingStockCondition();
-        for(int i = 0; i < 4; i++)
+        CheckingSlimeTypeQuantity();
+
+        for (int i = 0; i < StockButton.Length; i++)
         {
-            ItemButton[i].onClick.AddListener(Restock);
+            int closureIndex = i;
+            StockButton[closureIndex].onClick.AddListener(() => Restock(closureIndex));
+
+        }
+
+
+        for (int i = 0; i < 4; i++)
+        {
+            StockSelling_Text[i].text = StockSelling_Number[i].ToString("n0");
+            StockSellingPrice_Text[i].text = StockSellingPrice_Number[i].ToString("n0") + " " + "G";
+            RestockCost_Text[i].text = RestockCost_SlimeTypeCount[i].ToString("n0") + " " + SlimeType[i];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Buy(0);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Buy(1);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Buy(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Buy(3);
         }
     }
 
+    /// <summary>
+    /// Checking which lab is unlocked
+    /// and the related items will be unlocked also
+    /// </summary>
     void CheckingUnlockLab()
     {
         for (int i = 0; i < 4; i++)
         {
             if (GameManagerScript.UnlockLab[i] == true)
             {
-                ItemButton[i].gameObject.SetActive(true);
+                StockButton[i].gameObject.SetActive(true);
             }
             else
             {
-                ItemButton[i].gameObject.SetActive(false);
+                StockButton[i].gameObject.SetActive(false);
             }
         }
     }
 
+    /// <summary>
+    /// Checking whether the stock is still available.
+    /// Whether yes or no, the related details will be shown.
+    /// </summary>
     public void CheckingStockCondition()
     {
         for (int i = 0; i < 4; i++)
         {
             if (GameManagerScript.UnlockLab[i] == true)
             {
-                if (RemainStockNumber[i] > 0)
+                if (StockSelling_Number[i] > 0)
                 {
-                    ItemButton[i].image.color = new Color(1, 0.9215f, 0.6039f);
-                    ItemButton[i].interactable = false;
-                    ItemDetails_HaveStock[i].SetActive(true);
-                    ItemDetails_OutOfStock[i].SetActive(false);
+                    StockButton[i].image.color = new Color(1, 0.9215f, 0.6039f);
+                    StockButton[i].interactable = false;
+                    StockDetails_HaveStock[i].SetActive(true);
+                    StockDetails_OutOfStock[i].SetActive(false);
                     Debug.Log("HaveStock");
                 }
                 else
                 {
-                    ItemButton[i].image.color = new Color(1, 0.7568f, 0.2745f);
-                    ItemButton[i].interactable = true;
-                    ItemDetails_HaveStock[i].SetActive(false);
-                    ItemDetails_OutOfStock[i].SetActive(true);
+                    StockButton[i].image.color = new Color(1, 0.7568f, 0.2745f);
+                    StockButton[i].interactable = true;
+                    StockDetails_HaveStock[i].SetActive(false);
+                    StockDetails_OutOfStock[i].SetActive(true);
                     Debug.Log("NoStock");
                 }
             }
         }
     }
 
-    public void Restock()
+    /// <summary>
+    /// Use this function to restock the items if it is out of stock
+    /// </summary>
+    public void Restock(int buttonIndex)
+    {
+
+        if (StockSelling_Number[buttonIndex] <= 0)
+        {
+            if (GameManagerScript.SlimeTypeCount[buttonIndex] >= RestockCost_SlimeTypeCount[buttonIndex])
+            {
+                GameManagerScript.SlimeTypeCount[buttonIndex] -= RestockCost_SlimeTypeCount[buttonIndex];
+                StockSelling_Number[buttonIndex] = MaxStock_Number[buttonIndex];
+                StockSelling_Bar[buttonIndex].maxValue = MaxStock_Number[buttonIndex];
+                StockSelling_Bar[buttonIndex].value = MaxStock_Number[buttonIndex];
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Checking the Slime Type Quantity
+    /// and show in the market panel
+    /// </summary>
+    public void CheckingSlimeTypeQuantity()
     {
         for (int i = 0; i < 4; i++)
         {
-            if(RemainStockNumber[i] <= 0)
+
+            SlimeTypeQuantity_Text[i].text = GameManagerScript.SlimeTypeCount[i].ToString("n0");
+        }
+    }
+
+    /// <summary>
+    /// Use this function to purchase the item
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public bool Buy(int item)
+    {
+        if (StockDetails_HaveStock[item] == true)
+        {
+            if (StockSelling_Number[item] > 0)
             {
-                RemainStockNumber[i] = MaxStockNumber[i];
+                StockSelling_Number[item]--;
+                StockSelling_Bar[item].value--;
+                GameManagerScript.TotalCash += StockSellingPrice_Number[item];
             }
-        }
-    }
-
-    public void CheckingSlimeTypeQuantity()
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            SlimeTypeQuantityText[i].text = GameManagerScript.SlimeTypeCount[i].ToString();
-        }
-    }
-
-    public bool Buy (int item)
-    {
-
-        if (RemainStockNumber[item] > 0)
-        {
-            RemainStockNumber[item]--;
             return true;
         }
 
@@ -118,6 +183,6 @@ public class ShoppingSystem : MonoBehaviour
         {
             return false;
         }
-        
+
     }
 }
