89a780585561a6a1cd5739c0f1a6e0b76d33b381
changyowen <changyowen@gmail.com>
2020-09-14 00:26:55 +0800

    update pew2

--- SlimeCorp/Assets/Script/GameManagerScript.cs
+++ SlimeCorp/Assets/Script/GameManagerScript.cs
@@ -29,7 +29,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //LoadData();
+        LoadData();
     }
 
     // Update is called once per frame
