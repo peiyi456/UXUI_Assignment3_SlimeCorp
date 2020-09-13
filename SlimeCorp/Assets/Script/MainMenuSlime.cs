83bd9da4c3fec12121e12c4b6b16f3bf5041e70f
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-09-13 19:01:32 +0800

    update MainMenu (complete)

---
+++ SlimeCorp/Assets/Script/MainMenuSlime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSlime : MonoBehaviour
+{
+    float startCountdown;
+    float DelayedTime = 2.0f;
+    public GameObject BasicSlime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startCountdown = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 theScale = transform.localScale;
+        if (Time.time >= startCountdown + DelayedTime)
+        {
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
